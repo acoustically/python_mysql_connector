3a5362ecb499901a9b4cb8de362c89bd124280d7
acoustically <acoustically@kookmin.ac.kr>
2017-10-23 21:18:36 +0900

    change cursor from array to dictation

--- mysql_connector.py
+++ mysql_connector.py
@@ -17,36 +17,30 @@ class Connector:
 
   def query(self, sql):
     connection = self.__connect()
-    cursor = connection.cursor()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
     if self.__check_sql(sql):
       result, err = self.__manipulate_query(cursor, sql)
     else:
       result, err = self.__search_query(cursor, sql)
+    connection.commit()
     connection.close()
     return result, err
 
   def __search_query(self, cursor, sql):
     try:
-      print(1)
       row = cursor.execute(sql)
-      print(2)
       result = cursor.fetchall()
-      print(3)
       if row == 0:
-        print(4)
         return None, {"message":"not exist", "errno":1602}
       else:
-        print(5)
         return result, None
     except Exception as error:
-      print(6)
       return None, error
 
   def __manipulate_query(self, cursor, sql):
     try:
       result = cursor.execute(sql)
       cursor.fetchall()
-      connection.commit()
       return result, None
     except Exception as error:
       return None, error.args
